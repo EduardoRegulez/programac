5d2c1558742215d159003d0e02b1154fd670a4da
Humbi Regulez <Edu@Humbis-MacBook-Pro.local>
2016-01-06 18:48:04 +0100

    sudoku

--- trabajovacaciones/sudoku/sudoku.cpp
+++ trabajovacaciones/sudoku/sudoku.cpp
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define lados 9 
 int tabla[lados][lados];
 
 //generate random numbers to input
 //
+void generador(){
+
+    
+    for(int r=0;r<11;r++){
+
+    srand (time(NULL));
+    int random = rand() % 10;
+    int random2 = rand() % 10;
+    int randominput = rand() % 9;
+    randominput=randominput +1;
+
+    tabla[random][random2]=randominput;
+    for(int l =0;l<9;l++)
+    while(tabla[random][random2]== tabla[random][l]||tabla[random][random2]==tabla[l][random2])
+	int randominput = rand() % 9;
+    	tabla[random][random2]=randominput;
+	
+    }
+}
 void entrada( int col,int fila,int input){
 
     printf("columna? ");
@@ -12,6 +32,9 @@ void entrada( int col,int fila,int input){
     printf("fila? ");
     scanf("%i",&fila);
     printf("numero para introducir :");
+    scanf("%i",&input);
+    if(input>9||input<1)
+    printf("numero no acceptado introduce numero entre 1 y 9 :");
     scanf("%i",&input);
 
     tabla[col][fila] = input;
