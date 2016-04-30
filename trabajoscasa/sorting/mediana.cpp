fce50213a8b83909211c2c185e68ebef12f34338
Humbi Regulez <Edu@Humbis-MacBook-Pro.local>
2016-04-30 12:30:59 +0200

    mediana

---
+++ trabajoscasa/sorting/mediana.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define S 9
+
+int main(){
+
+    int numeros[9]={3, 7, 1, 9, 2, 1, 4, 2, 5};
+
+
+
+    int t;
+    for (int i = 0; i < S; i++){ 
+	for (int j = 0; j < S - 1; j++) {
+	    if (numeros[j]>numeros[j + 1]) {
+		t = numeros[j];
+		numeros[j] = numeros[j + 1];
+		numeros[j + 1] = t;
+	    }}}
+
+
+
+    printf("orden es : ");
+    for (int i = 0; i < S; i++)
+	printf("%i \t",numeros[i]);
+
+    printf("mediana es = %i \n",numeros[ S/2]);
+
+
+
+    return EXIT_SUCCESS;
+}
